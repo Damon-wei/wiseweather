9a158dbd52f25107b960d210b99eb6a37a6cf3eb
Damon-wei <18612124664@163.com>
2017-04-07 14:57:50 +0800

    加入创建数据库和表的各项配置

---
+++ app/src/main/java/com/wisedeve/wiseweather/db/Province.java
@@ -0,0 +1,38 @@
+package com.wisedeve.wiseweather.db;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * Created by Damon on 2017/4/7.
+ * Description :
+ */
+
+public class Province extends DataSupport {
+    private int id;
+    private int provinceCode;
+    private String provinceName;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getProvinceCode() {
+        return provinceCode;
+    }
+
+    public void setProvinceCode(int provinceCode) {
+        this.provinceCode = provinceCode;
+    }
+
+    public String getProvinceName() {
+        return provinceName;
+    }
+
+    public void setProvinceName(String provinceName) {
+        this.provinceName = provinceName;
+    }
+}
